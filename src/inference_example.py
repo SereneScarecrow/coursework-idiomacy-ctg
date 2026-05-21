c783506988eb615fb5c9cb7f54e0b7db3b6496bb
SereneScarecrow <aazhigulina@edu.hse.ru>
2026-05-22 00:20:13 +0300

    updated: PrompConstructor usage in inference_example.py

--- src/inference_example.py
+++ src/inference_example.py
@@ -1,16 +1,17 @@
 from dotenv import load_dotenv
 from langfuse.langchain import CallbackHandler
 
-from model_constructor import ModelConstructor
-from model_inference import PromptConstructor, ModelInference
+from inference.model_constructor import ModelConstructor
+from inference.model_inference import PromptConstructor, ModelInference
 
 load_dotenv()
 
 client = ModelConstructor.create_client("GigaChat-2", "gigachat")
-prompt = PromptConstructor.get_prompt("test", {"word": 'hello'})
-
-print(prompt)
-
 inference = ModelInference(client)
-print(inference(prompt))
+
+prompt_builder = PromptConstructor("test")
+formated_prompt = prompt_builder.render({"word": 'hello'})
+
+print(formated_prompt)
+print(inference(formated_prompt))
 
