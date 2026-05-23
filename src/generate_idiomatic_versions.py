728e72d517e277b1e0a6aacb3394beb8786a9143
SereneScarecrow <aazhigulina@edu.hse.ru>
2026-05-23 03:22:27 +0300

    tired

--- src/generate_idiomatic_versions.py
+++ src/generate_idiomatic_versions.py
@@ -10,7 +10,7 @@ from inference.model_inference import PromptConstructor, ModelInference
 
 load_dotenv()
 
-def extract_literal_versions(model_name, provider, prompt, df, columns_mapping,
+def generate_idiomatic_versions(model_name, provider, prompt, df,
                              checkpoint_path="checkpoint.csv", resume=True):
     """
     Функция для извлечения буквальных версий текстов с чекпоинтами.
@@ -25,7 +25,7 @@ def extract_literal_versions(model_name, provider, prompt, df, columns_mapping,
     # загрузка промпта
     prompt_builder = PromptConstructor(prompt)
     # рендер промптов с переменными
-    texts_with_prompts = prompt_builder.render_from_df(df, columns_mapping=columns_mapping)
+    texts_with_prompts = prompt_builder.render_from_df(df)
 
     # загрузка чекпоинта
     results = []
@@ -71,31 +71,14 @@ def extract_literal_versions(model_name, provider, prompt, df, columns_mapping,
     
     return pd.Series(results)
 
-data = pd.read_csv("data/dataset_maxi.csv")
-data_literature = data[data["text_domen"] == "художественный"]
-data_news = data[data["text_domen"] == "новостной"]
+data = pd.read_csv("data/dataset_maxi_literal.csv").sample(n=5)
 
 # извлечение буквальных художественных текстов
-literature_literal = extract_literal_versions(
-    model_name="GigaChat-2-Max",
-    provider="gigachat",
-    prompt="literal_v6",
-    df=data_literature[["original_text", "idiomatic_expressions"]],
-    columns_mapping={"original_text": "text", "idiomatic_expressions": "idioms"},
-    checkpoint_path="checkpoints/checkpoint_literal.csv")
+idiomatic_versions = generate_idiomatic_versions(
+    model_name="gemma3:4b",
+    provider="ollama",
+    prompt="idiomatic_high",
+    df=data["literal_version"],
+    checkpoint_path="checkpoints/checkpoint_gemma3_1.csv")
 
-# извлечение буквальных новостных текстов
-news_literal = extract_literal_versions(
-    model_name="GigaChat-2-Pro",
-    provider="gigachat",
-    prompt="literal_v6",
-    df=data_news[["original_text", "idiomatic_expressions"]],
-    columns_mapping={"original_text": "text", "idiomatic_expressions": "idioms"},
-    checkpoint_path="checkpoints/checkpoint_news.csv") 
-
-# мерджинг и сохранение
-all_literal_versions = pd.concat([literature_literal, news_literal], ignore_index=True)
-all_literal_versions.to_csv("data/literal.csv", index=False)
-
-data['literal_version'] = all_literal_versions
-data.to_csv("data/dataset_maxi_literal.csv")
+print(idiomatic_versions)
